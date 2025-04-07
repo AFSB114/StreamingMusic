8512a2025051b2443ac20bcd48cc8a25bc3f2b5a
AFSB114 <asuazab13@gmail.com>
2025-04-07 10:58:28 -0500

    Add module record label

---
+++ backend/src/main/java/com/sena/basic_crud/specification/RecordLabelSpecification.java
@@ -0,0 +1,18 @@
+package com.sena.basic_crud.specification;
+
+import com.sena.basic_crud.model.RecordLabel;
+import org.springframework.data.jpa.domain.Specification;
+
+public class RecordLabelSpecoification {
+    public static Specification<RecordLabel> hasName(String name) {
+        return (root, query, criteriaBuilder) ->
+                name == null ? criteriaBuilder.conjunction() :
+                        criteriaBuilder.like(criteriaBuilder.lower(root.get("name")), "%" + name.toLowerCase() + "%");
+    }
+
+    public static Specification<RecordLabel> hasCountry(String country) {
+        return (root, query, criteriaBuilder) ->
+                type == null ? criteriaBuilder.conjunction() :
+                        criteriaBuilder.equal(criteriaBuilder.lower(root.get("country")), country.toLowerCase());
+    }
+}
